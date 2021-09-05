67ff65492644e402064265263145c208d40210a5
blackpanther1927-design <83531534+blackpanther1927-design@users.noreply.github.com>
2021-09-05 12:48:30 +0530

    Add files via upload

---
+++ main..js
@@ -0,0 +1,9 @@
+function setup() {
+    canvas=createCanvas(300 , 300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300, 300);
+
+    poseNet=ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
